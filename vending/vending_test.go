f9d209a2a855ec4a4b9c56edc548b036af78925a
sck shuhari <sckshuhari@scks-MacBook-Pro.local>
2019-08-21 12:01:12 +0700

    [Added]: เพิ่มเทส Test_BuyDrink_Input_TotalBalance_8_Item_DW_Should_Be_Item_DW_Balance_0 ในไฟล์ vending_test.go

--- vending/vending_test.go
+++ vending/vending_test.go
@@ -1,1 +1,22 @@
-package vending_test
+package vending
+
+import (
+	"testing"
+)
+
+func Test_BuyDrink_Input_TotalBalance_8_Item_DW_Should_Be_Item_DW_Balance_0(t *testing.T) {
+	expetedItem, expetedBalance := "DW", 0
+	item := "DW"
+	vending := VendingMachine{
+		TotalBalance: 8,
+	}
+
+	actualItem, actualBalance := vending.BuyDrink(item)
+
+	if expetedItem != actualItem {
+		t.Errorf("Expect %s but got %s", expetedItem, actualItem)
+	}
+	if expetedBalance != actualBalance {
+		t.Errorf("Expect %d but got %d", expetedBalance, actualBalance)
+	}
+}
