9e392616d4396556cb21e5cdebbc0c06439f8b1b
endkarn <end_karn@hotmail.com>
2019-08-21 12:11:26 +0700

    [Added]: เพิ่ม UnitTest ของ calculateChange ที่ vending/vending_test.go

--- vending/vending_test.go
+++ vending/vending_test.go
@@ -15,3 +15,17 @@ func Test_CalculateChange_Input_TotalBalance_20_DrinkPrice_15_Should_Be_5(t *tes
 		t.Errorf("Expect %d but got %d", expect, actual)
 	}
 }
+
+func Test_CalculateChange_Input_TotalBalance_8_DrinkPrice_8_Should_Be_0(t *testing.T) {
+	expect := 0
+	vendingMachine := VendingMachine{
+		TotalBalance: 8,
+	}
+	dirnkPrice := 8
+
+	actual := vendingMachine.calculateChange(dirnkPrice)
+
+	if expect != actual {
+		t.Errorf("Expect %d but got %d", expect, actual)
+	}
+}
